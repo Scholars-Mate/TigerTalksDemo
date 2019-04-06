840a8ffb159399d1bc99c392cab156cc9cdba830
Wong <crwgg2@mizzou.edu>
2019-04-06 13:59:47 -0500

    Added greeting

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+@app.route('/<name>')
+def hello_world(name=None):
+	return 'Hello {}!'.format(name)
